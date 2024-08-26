87b86bd19a86ed0ea4526902cce44b28d140cdab
Nancy Israel <126035946+Nancyisrael@users.noreply.github.com>
2024-08-26 12:46:32 +0530

    Add files via upload

---
+++ F4.MINMAX IN ARRAY.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+// Function to find the minimum element in an array
+int findMin(int arr[], int size) {
+    int min = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+// Function to find the maximum element in an array
+int findMax(int arr[], int size) {
+    int max = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter " << size << " elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    int minElement = findMin(arr, size);
+    int maxElement = findMax(arr, size);
+
+    cout << "Minimum element: " << minElement << endl;
+    cout << "Maximum element: " << maxElement << endl;
+
+    return 0;
+}
+
